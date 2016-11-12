42b3afce525602632137532d8c3760e2e2ddb93a
nbrons <nickbrons@msn.com>
2016-11-11 17:07:45 -0700

    Added new views, buttons

---
+++ app/src/main/java/cpsc481/t02/animalshelter/ExistingList.java
@@ -0,0 +1,13 @@
+package cpsc481.t02.animalshelter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExistingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_existing_list);
+    }
+}
--- app/src/main/java/cpsc481/t02/animalshelter/StartActivity.java
+++ app/src/main/java/cpsc481/t02/animalshelter/StartActivity.java
@@ -1,13 +1,30 @@
 package cpsc481.t02.animalshelter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button newAnimal;
+    private Button existingAnimal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        newAnimal = (Button)findViewById(R.id.newButton);
+        existingAnimal = (Button) findViewById(R.id.existing);
+
+        existingAnimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ExistingList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
