374fb66325ad9e9c67ad463056fa9296b731764a
maria-ma <mmamacla@ucalgary.ca>
2016-11-23 11:46:46 -0700

    added up navigation, changed icon, modified start screen, some formatting in surrendered and existing list view

--- app/src/main/java/cpsc481/t02/animalshelter/SingleAnimalView.java
+++ app/src/main/java/cpsc481/t02/animalshelter/SingleAnimalView.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,6 +18,9 @@ public class SingleAnimalView extends AppCompatActivity {
         setContentView(R.layout.activity_single_animal_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         TextView tag = (TextView) findViewById(R.id.single_tag);
 
